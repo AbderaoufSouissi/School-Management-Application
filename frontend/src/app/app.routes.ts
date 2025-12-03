360f14f085312ed8ebcf61ddff1a73fef622c595
Abderaouf Souissi <abderaouf.souissi@outlook.com>
2025-12-03 16:22:53 +0100

    frontend: add routing and styling config

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,62 @@
 import { Routes } from '@angular/router';
+import { authGuard, guestGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'students',
+    pathMatch: 'full'
+  },
+  {
+    path: 'auth',
+    children: [
+      {
+        path: 'login',
+        canActivate: [guestGuard],
+        loadComponent: () => 
+          import('./pages/auth/login/login.component').then(m => m.LoginComponent)
+      },
+      {
+        path: 'register',
+        canActivate: [guestGuard],
+        loadComponent: () => 
+          import('./pages/auth/register/register.component').then(m => m.RegisterComponent)
+      },
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: 'students',
+    canActivate: [authGuard],
+    children: [
+      {
+        path: '',
+        loadComponent: () => 
+          import('./pages/students/student-list/student-list.component').then(m => m.StudentListComponent)
+      },
+      {
+        path: 'create',
+        loadComponent: () => 
+          import('./pages/students/student-create/student-create.component').then(m => m.StudentCreateComponent)
+      },
+      {
+        path: ':id',
+        loadComponent: () => 
+          import('./pages/students/student-detail/student-detail.component').then(m => m.StudentDetailComponent)
+      },
+      {
+        path: ':id/edit',
+        loadComponent: () => 
+          import('./pages/students/student-edit/student-edit.component').then(m => m.StudentEditComponent)
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'students'
+  }
+];
